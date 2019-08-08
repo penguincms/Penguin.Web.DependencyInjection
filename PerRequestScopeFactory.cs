2b92794f8d9400f7bb49a10d56cb65ed599f2503
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:43:27 -0700

    Add files via upload

---
+++ PerRequestScopeFactory.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Penguin.Web.Testing;
+
+namespace Penguin.Web.DependencyInjection
+{
+    /// <summary>
+    /// Generates a new PerRequestScope for web request scoped DI
+    /// </summary>
+    public class PerRequestScopeFactory : IServiceScopeFactory
+    {
+        #region Constructors
+        /// <summary>
+        /// Constructs a new instance of this factory
+        /// </summary>
+        public PerRequestScopeFactory()
+        {
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// Returns a Dummy request scope for use in unit testing, with mock objects representing things like Session
+        /// </summary>
+        /// <returns>A Dummy request scope for use in unit testing, with mock objects representing things like Session</returns>
+        public static PerRequestServiceScope CreateDummy()
+        {
+            PerRequestServiceScope ServiceScope = new PerRequestServiceScope();
+            DefaultHttpContext context = new DefaultHttpContext
+            {
+                Session = new MockHttpSession()
+            };
+            ServiceScope.RequestProvider.Add(context);
+            context.RequestServices = ServiceScope.ServiceProvider;
+            return ServiceScope;
+        }
+
+        /// <summary>
+        /// Constructs a new Request based service scope
+        /// </summary>
+        /// <returns>A new Request based service scope</returns>
+        public IServiceScope CreateScope()
+        {
+            return new PerRequestServiceScope();
+        }
+
+        #endregion Methods
+    }
+}
---
+++ PerRequestServiceScope.cs
@@ -0,0 +1,11 @@
+﻿using Penguin.DependencyInjection.ServiceScopes;
+
+namespace Penguin.Web.DependencyInjection
+{
+    /// <summary>
+    /// All instances in this scope are disposed when the scope is disposed. May represent a single web request, or be used in other ways. All instances are Engine instance specific
+    /// </summary>
+    public class PerRequestServiceScope : ScopedServiceScope
+    {
+    }
+}
